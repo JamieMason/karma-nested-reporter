a5ea7fc0cd893d8c95b79cb70ad9eca0ef1b72de
Jamie Mason <siunmaru@gmail.com>
2014-07-19 18:01:17 +0100

    Fix issue where the _browsers array could not be found.

--- index.js
+++ index.js
@@ -1,165 +1,105 @@
+var util = require('util');
+
+function NestedReporter(extendBaseReporter, formatError, config, options) {
+
+  var self = this;
+
+  extendBaseReporter(self);
+
+  // Allow ourselves to call superclass methods.
+  self._super = {
+    specFailure: self.specFailure.bind(self)
+  };
+
+  options = options || {};
+  options.icon = options.icon || {};
+  options.icon.failure = options.icon.failure || '✘ ';
+  options.icon.indent = options.icon.indent || 'ட ';
+  options.color = options.color || {};
+  options.color.should = options.color.should || 'red';
+  options.color.browser = options.color.browser || 'yellow';
+
+  if (config.colors) {
+    require('colors');
+    self.USE_COLORS = true;
+    self.LOG_SINGLE_BROWSER = '%s: ' + '%s'.cyan + '\n';
+    self.LOG_MULTI_BROWSER = '%s %s: ' + '%s'.cyan + '\n';
+    self.SPEC_FAILURE = '%s %s FAILED'.red + '\n';
+    self.SPEC_SLOW = '%s SLOW %s: %s'.yellow + '\n';
+    self.ERROR = '%s ERROR'.red + '\n';
+    self.FINISHED_ERROR = ' ERROR'.red;
+    self.FINISHED_SUCCESS = ' SUCCESS'.green;
+    self.FINISHED_DISCONNECTED = ' DISCONNECTED'.red;
+    self.X_FAILED = ' (%d FAILED)'.red;
+    self.TOTAL_SUCCESS = 'TOTAL: %d SUCCESS'.green + '\n';
+    self.TOTAL_FAILED = 'TOTAL: %d FAILED, %d SUCCESS'.red + '\n';
+  }
+
+  self.onBrowserComplete = function() {
+    self.write(self._refresh());
+  };
+
+  self.onBrowserStart = function(browser) {
+    self._browsers.push(browser);
+    if (self._isRendered) {
+      self.write('\n');
+    }
+    self.write(self._refresh());
+  };
+
+  self.onRunStart = function() {
+    self._browsers = [];
+    self._isRendered = false;
+  };
+
+  self.specFailure = function(browser, result) {
+    var depth = result.suite.length;
+    return self._super.specFailure.call(self, browser, {
+      log: result.log,
+      description: '\n' + self._repeat(depth, '  ') + options.icon.indent + options.icon.failure + result.description,
+      suite: result.suite.map(function(description, i) {
+        return '\n' + self._repeat(i, '  ') + options.icon.indent + description;
+      })
+    });
+  };
+
+}
+
+NestedReporter.prototype._repeat = function(n, str) {
+  var res = [];
+  var i;
+  for (i = 0; i < n; ++i) {
+    res.push(str);
+  }
+  return res.join('');
+};
+
+NestedReporter.prototype._remove = function() {
+  var cmd = '';
+  if (!this._isRendered) {
+    return '';
+  }
+  this._browsers.forEach(function() {
+    cmd += '\x1B[1A' + '\x1B[2K';
+  });
+  this._isRendered = false;
+  return cmd;
+};
+
+NestedReporter.prototype._render = function() {
+  this._isRendered = true;
+  return this._browsers.map(this.renderBrowser).join('\n') + '\n';
+};
+
+NestedReporter.prototype._refresh = function() {
+  return this._remove() + this._render();
+};
+
+// Register with Karma
+// ---------------------------------------------------------------------------
+
+NestedReporter.$inject = ['baseReporterDecorator', 'formatError', 'config', 'config.nestedReporter'];
+
 module.exports = {
-  'reporter:nested': ['type', (function() {
-
-    // @TODO: toggle based on colors value in config.
-    var colors = require('colors');
-
-    var NestedReporter = function(baseReporterDecorator, config) {
-
-      // ------------------------------------
-      // CONFIGURATION
-      // ------------------------------------
-
-      var pluginConfig = config.nestedReporter || {};
-      var enableColors = config.colors !== false;
-
-      pluginConfig.icon = pluginConfig.icon || {};
-      pluginConfig.icon.failure = pluginConfig.icon.failure || '✘ ';
-      pluginConfig.icon.indent = pluginConfig.icon.indent || 'ட ';
-      pluginConfig.icon.browser = pluginConfig.icon.browser || '';
-
-      pluginConfig.color = pluginConfig.color || {};
-      pluginConfig.color.should = pluginConfig.color.should || 'red';
-      pluginConfig.color.browser = pluginConfig.color.browser || 'yellow';
-
-      // ------------------------------------
-      // PRIVATE
-      // ------------------------------------
-
-      /**
-       * Repeat the same string n number of times.
-       *
-       * @param  {Number} n
-       * @param  {String} str
-       * @return {String}
-       */
-      function repeat(n, str) {
-        var res = [];
-        var i;
-        for (i = 0; i < n; ++i) {
-          res.push(str);
-        }
-        return res.join('');
-      }
-
-      var write = {
-
-        /**
-         * Render a describe('when ...').
-         *
-         * @param  {Number} depth
-         * @param  {String} pathName
-         * @return {String}
-         */
-        when: function(depth, pathName) {
-          var icon = depth > 0 ? pluginConfig.icon.indent : '';
-          return icon + pathName;
-        },
-
-        /**
-         * Render an it('should ...').
-         *
-         * @param  {Number} depth
-         * @param  {String} pathName
-         * @return {String}
-         */
-        should: function(depth, pathName) {
-          var output = pluginConfig.icon.failure + pathName;
-          return enableColors ? output[pluginConfig.color.should] : output;
-        },
-
-        /**
-         * Render a captured Browser's name.
-         *
-         * @param  {Number} depth
-         * @param  {String} pathName
-         * @return {String}
-         */
-        browser: function(depth, pathName) {
-          var output = pluginConfig.icon.browser + pathName;
-          return enableColors ? output[pluginConfig.color.browser] : output;
-        },
-
-        /**
-         * Recursively write.failures through our nested object of Whens, Shoulds and Browsers and render
-         * each line out to the terminal.
-         *
-         * @param  {Object} path
-         * @param  {Number} depth
-         */
-        failures: function(path, depth) {
-          for (var property in path) {
-            if (path.hasOwnProperty(property)) {
-              console.log(repeat(depth, '  ') + property);
-              write.failures(path[property], depth + 1);
-            }
-          }
-        }
-
-      };
-
-      // ------------------------------------
-      // PUBLIC
-      // ------------------------------------
-
-      // extend the base reporter
-      baseReporterDecorator(this);
-
-      /**
-       * Called each time a test fails in a given Browser.
-       *
-       * @param  {[type]} browser [description]
-       * @param  {[type]} result  [description]
-       * @return {[type]}         [description]
-       */
-      this.specFailure = function(browser, result) {
-
-        var path = [].concat(result.suite, result.description, browser.name);
-        var maxDepth = path.length - 1;
-
-        // recursively walk down the object tree creating any new steps as needed.
-        path.reduce(function(branch, pathName, depth) {
-
-          var writer;
-          var pathExists;
-
-          if (depth < maxDepth - 1) {
-            writer = 'when';
-          } else if (depth === maxDepth - 1) {
-            writer = 'should';
-          } else if (depth === maxDepth) {
-            writer = 'browser';
-          }
-
-          // key each entry by the indented, formatted output we'll display.
-          pathName = write[writer](depth, pathName);
-
-          pathExists = pathName in branch;
-
-          if (!pathExists) {
-            branch[pathName] = {};
-          }
-
-          // return the new/current branch to walk down into.
-          return branch[pathName];
-
-        }, this.describeTree);
-
-      };
-
-      this.onRunStart = function(browsers, results) {
-        this.describeTree = {};
-      };
-
-      this.onRunComplete = function(browsers, results) {
-        write.failures(this.describeTree, -1);
-      };
-
-    };
-
-    NestedReporter.$inject = ['baseReporterDecorator', 'config'];
-
-    return NestedReporter;
-
-  }())]
+  'reporter:nested': ['type', NestedReporter]
 };
